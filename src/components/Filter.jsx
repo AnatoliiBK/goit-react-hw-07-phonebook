b616550e8bb792ba10e9109571e8fa4debeffafd
AnetRo <beatrice.ccq@gmail.com>
2023-07-26 23:28:46 +0300

    del commits

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -25,27 +25,6 @@ export const Filter = () => {
   );
 };
 
-
-
-// import PropTypes from 'prop-types';
-// import { labelStyle, inputStyle } from './styles';
-
-// export const Filter = ({ handleChange }) => {
-  
-//   return (
-//     <label style={labelStyle}>
-//       Find contacts by name
-//       <input
-//         type="text"
-//         name="filter"       
-//         onChange={handleChange}
-//         style={inputStyle}
-//       />
-//     </label>
-//   );
-// };
-
-
 Filter.propTypes = {
   filter: PropTypes.string,
   handleChange: PropTypes.func.isRequired,
