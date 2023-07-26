52d01bcb1d29c180bd3d6dd35750b03c4bd35049
AnetRo <beatrice.ccq@gmail.com>
2023-07-26 21:55:18 +0300

    del comm

--- src/components/redux/contactsSlice.js
+++ src/components/redux/contactsSlice.js
@@ -1,41 +1,20 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-// export const fetchContacts = createAsyncThunk(
-//   'contacts/fetchAll',
-//   async () => {
-//     const response = await axios.get('https://64b3a5a00efb99d862683852.mockapi.io/contacts');
-//     return response.data;
-//   }
-// );
-
-// export const fetchContacts = createAsyncThunk(
-//   'contacts/fetchAll',
-//   async (_, { getState }) => {
-//     const state = getState();
-//     if (state.contacts.items.length > 0) {
-//       // Clear the existing contacts before fetching new data
-//       state.contacts.items = [];
-//     }
-
-//     const response = await axios.get('https://64b3a5a00efb99d862683852.mockapi.io/contacts');
-//     return response.data;
-//   }
-// );
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
   async (_, { getState, dispatch }) => {
     const state = getState();
     if (state.contacts.items.length > 0) {
-      // Clear the existing contacts before fetching new data
+      
       state.contacts.items = [];
     }
-
+    await syncDeletedContacts();
     const response = await axios.get('https://64b3a5a00efb99d862683852.mockapi.io/contacts');
     const fetchedContacts = response.data;
 
-    // Check for duplicates and remove them
+    
     const uniqueContacts = fetchedContacts.reduce((acc, contact) => {
       const existingContact = acc.find((c) => c.number === contact.number);
       if (!existingContact) {
@@ -44,35 +23,12 @@ export const fetchContacts = createAsyncThunk(
       return acc;
     }, []);
 
-    // Update the contacts list in Redux store
+    
     dispatch(fetchContacts.fulfilled(uniqueContacts));
 
     return uniqueContacts;
   }
 );
-
-
-
-// export const addContact = createAsyncThunk(
-//   'contacts/addContact',
-//   async (newContact, { getState, rejectWithValue }) => {
-//     const { contacts } = getState();
-//     const existingContact = contacts.items.find(
-//       (contact) => contact.number === newContact.number
-//     );
-//     if (existingContact) {
-//       alert(`Contact with phone number ${newContact.number} already exists.`);
-//       return rejectWithValue('Contact already exists');
-//     }
-
-//     try {
-//       const response = await axios.post('https://64b3a5a00efb99d862683852.mockapi.io/contacts', newContact);
-//       return response.data;
-//     } catch (error) {
-//       return rejectWithValue('Failed to add contact.');
-//     }
-//   }
-// );
 
 export const addContact = createAsyncThunk(
   'contacts/addContact',
@@ -89,66 +45,37 @@ export const addContact = createAsyncThunk(
     const response = await axios.post('https://64b3a5a00efb99d862683852.mockapi.io/contacts', newContact);
     return response.data;
     
-    // await axios.post('https://64b3a5a00efb99d862683852.mockapi.io/contacts', newContact);
-
-    // const updatedResponse = await axios.get('https://64b3a5a00efb99d862683852.mockapi.io/contacts');
-    // return updatedResponse.data;
+   
   }
 );
 
+const syncDeletedContacts = async () => {
+  
+  const deletedContacts = JSON.parse(localStorage.getItem('deletedContacts')) || [];
+  
+  if (deletedContacts.length > 0) {
+    for (const contactId of deletedContacts) {
+      await deleteContact(contactId);
+    }
+   
+    localStorage.setItem('deletedContacts', JSON.stringify([]));
+  }
+};
 
-
-// export const addContact = createAsyncThunk(
-//   'contacts/addContact',
-//   async (newContact, { getState, rejectWithValue }) => {
-//     const { contacts } = getState();
-//     const existingContact = contacts.items.find(
-//       (contact) => contact.number === newContact.number
-//     );
-//     if (existingContact) {
-//       return rejectWithValue(`Contact with phone number ${newContact.number} already exists.`);
-//     }
-
-//     try {
-//       const response = await axios.post('https://64b3a5a00efb99d862683852.mockapi.io/contacts', newContact);
-//       return response.data;
-//     } catch (error) {
-//       return rejectWithValue('Failed to add contact.');
-//     }
-//   }
-// );
-
-
-// export const addContact = createAsyncThunk(
-//   'contacts/addContact',
-//   async (newContact, { getState }) => {
-//     const state = getState();
-//     const existingContact = state.contacts.items.find(
-//       (contact) => contact.number === newContact.number
-//     );
-//     if (existingContact) {
-//       throw new Error(`Contact with phone number ${newContact.number} already exists.`);
-//     }
-
-//     const response = await axios.post('https://64b3a5a00efb99d862683852.mockapi.io/contacts', newContact);
-//     return response.data;
-//   }
-// );
-// export const addContact = createAsyncThunk(
-//   'contacts/addContact',
-//   async (newContact) => {
-//     const response = await axios.post('https://64b3a5a00efb99d862683852.mockapi.io/contacts', newContact);
-//     return response.data;
-//   }
-// );
 
 export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
   async (contactId) => {
+    
     await axios.delete(`https://64b3a5a00efb99d862683852.mockapi.io/contacts/${contactId}`);
+    
+    const deletedContacts = JSON.parse(localStorage.getItem('deletedContacts')) || [];
+    localStorage.setItem('deletedContacts', JSON.stringify([...deletedContacts, contactId]));
+    
     return contactId;
   }
 );
+
 
 const initialState = Object.freeze({
   contacts: {
@@ -212,55 +139,3 @@ const contactsSlice = createSlice({
 export const { setFilter } = contactsSlice.actions;
 
 export default contactsSlice.reducer;
-
-
-
-
-// import { createSlice } from '@reduxjs/toolkit';
-// // import { parsePhoneNumberFromString } from 'libphonenumber-js';
-
-// const initialState = {
-//   contacts: [],
-//   filter: '',
-// };
-
-// const contactsSlice = createSlice({
-//   name: 'contacts',
-//   initialState,
-//   reducers: {
-    
-//     addContact: (state, action) => {
-//       const { id, name, number } = action.payload;
-
-//       if (!name || !number) {
-//         return;
-//       }  
-
-//     // const phoneNumber = parsePhoneNumberFromString(number, 'UA');
-//     //   const formattedNumber = phoneNumber.formatInternational();
-
-//     //   const duplicateContact = state.contacts.find(
-//     //     (contact) => contact.number === formattedNumber
-//     //   );
-//     //   if (duplicateContact) {
-//     //     alert(`Контакт з номером телефону ${formattedNumber} вже є в списку`);
-//     //     return;
-//     //   }
-//       state.contacts.push({ id, name });
-//     },
-
-//     deleteContact: (state, action) => {
-//       state.contacts = state.contacts.filter(
-//         (contact) => contact.id !== action.payload
-//       );
-//     },
-
-//     setFilter: (state, action) => {
-//       state.filter = action.payload;
-//     },
-//   },
-// });
-
-// export const { addContact, deleteContact, setFilter } = contactsSlice.actions;
-
-// export default contactsSlice.reducer;
--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -4,32 +4,3 @@ import contactsReducer from "./contactsSlice";
 export const store = configureStore({
   reducer: contactsReducer,
 });
-
-
-
-
-
-
-// import { configureStore } from '@reduxjs/toolkit';
-// import contactsReducer from "./contactsSlice";
-// import { persistStore, persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
-
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-// };
-
-// const persistedReducer = persistReducer(persistConfig, contactsReducer);
-
-// const store = configureStore({
-//   reducer: persistedReducer,
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: false, // Вимкнення перевірки серіалізованості для дій
-//     }),
-// });
-
-// const persistor = persistStore(store);
-
-// export { store, persistor };
