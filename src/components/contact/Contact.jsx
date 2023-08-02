7f5862b34e4df4f1e88ec2b88a194b2270d5d4cf
AnetRo <beatrice.ccq@gmail.com>
2023-08-02 18:09:57 +0300

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import axios from 'axios';
+// import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { deleteContact } from 'components/redux/contactsSlice';
 import { nameStyle, name, btnDelStyle } from 'components/styles';
@@ -9,7 +9,7 @@ export const ContactItem = ({ contact }) => {
 
   const handleDelete = async () => {
     try {      
-      await axios.get(`https://64b3a5a00efb99d862683852.mockapi.io/contacts/${contact.id}`);     
+      // await axios.get(`https://64b3a5a00efb99d862683852.mockapi.io/contacts/${contact.id}`);     
       dispatch(deleteContact(contact.id));
       console.log("ВИДАЛЕННЯ", contact, contact.id )
     } catch (error) {
